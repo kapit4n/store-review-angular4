cd78b8e423c838710c6dc05a49f2f56cf0a0dc18
Luis Arce <luis.arce22@gmail.com>
2017-10-18 10:05:25 -0400

    Added stock-item and home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { StockItemComponent } from './stock-item/stock-item.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+    {
+        path: 'stock-item',
+        component: StockItemComponent,
+    },{
+        path: '',
+        component: HomeComponent,
+    },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StockItemComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
