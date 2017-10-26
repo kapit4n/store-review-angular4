c8736bd2c5727b71d01c843223db294bc01ef9db
larce <luis.arce22@gmail.com>
2017-10-26 07:36:55 -0400

    Mat-table integration with datasource from API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpModule } from '@angular/http';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule, MatCheckboxModule, MatTableModule} from '@angular/material';
 
+
 import { StockServiceService } from './stock-service.service'
 import { AppComponent } from './app.component';
 import { StockItemComponent } from './stock-item/stock-item.component';
